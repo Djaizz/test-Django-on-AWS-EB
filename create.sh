a64bf3763b6953ab095b860222be478c901d304c
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-04 23:51:52 -0500

    update create.sh

--- create.sh
+++ create.sh
@@ -4,7 +4,7 @@ python DjangoProject/manage.py syncdb
 python DjangoProject/manage.py collectstatic
 
 git add --all
-git commit -m "create AWS EB app"
+git commit -m "create AWS EB Django app"
 git push --all
 
 eb create --no-verify-ssl
