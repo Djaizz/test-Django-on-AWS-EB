26663a6ca0c87077150dbc137bcb34c05d22e413
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-04 23:49:24 -0500

    create AWS EB app

---
+++ create.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python DjangoProject/manage.py syncdb
+python DjangoProject/manage.py collectstatic
+
+git add --all
+git commit -m "create AWS EB app"
+git push --all
+
+eb create --no-verify-ssl
