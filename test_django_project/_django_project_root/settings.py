b63fafdccb68004cf8fcbf90d46479a5f8983db6
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-04-18 17:57:33 -0700

    add static files

--- test_django_project/_django_project_root/settings.py
+++ test_django_project/_django_project_root/settings.py
@@ -101,3 +101,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images): https://docs.djangoproject.com/en/3.0/howto/static-files
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
