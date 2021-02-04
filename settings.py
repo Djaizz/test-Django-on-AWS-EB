c32e8c5b81cd19ba0511d8b2f74c8e008c95cebd
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-02-04 11:14:31 -0800

    update settings.STATIC_ROOT

--- settings.py
+++ settings.py
@@ -99,5 +99,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files
-STATIC_ROOT = 'static'
+STATIC_ROOT = '.staticfiles'
 STATIC_URL = '/static/'
